ad19662f73560ba95f3b58587c2bb73cca8b611b
Antxon Eguiguren <antxonegi@gmail.com>
2020-04-13 13:20:06 +0200

    Header component created and styled

--- src/App.js
+++ src/App.js
@@ -3,20 +3,24 @@ import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shoppage.component';
+import Header from './components/header/header.component';
 
 import './App.css';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path='/' component={HomePage} />
-      <Route exact path='/shop' component={ShopPage} />
-      <Route path='/shop/hats' />
-      <Route path='/shop/jackets' />
-      <Route path='/shop/sneakers' />
-      <Route path='/shop/womens' />
-      <Route path='/shop/mens' />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+        <Route path='/shop/hats' />
+        <Route path='/shop/jackets' />
+        <Route path='/shop/sneakers' />
+        <Route path='/shop/womens' />
+        <Route path='/shop/mens' />
+      </Switch>
+    </div>
   );
 }
 
