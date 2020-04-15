51c5bbe16c71bb58050de9c0c013c7c3ca6dedb0
Antxon Eguiguren <antxonegi@gmail.com>
2020-04-15 15:39:23 +0200

    CartIcon component created and included in the HeaderComponent

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,6 +6,8 @@ import { auth } from '../../firebase/firebase.utils';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
+import CartIcon from '../cart-icon/cart-icon.component';
+
 import './header.styles.scss';
 
 const Header = ({ currentUser }) => (
@@ -26,6 +28,7 @@ const Header = ({ currentUser }) => (
                     :
                     <Link className='option' to='/sign-in'>SIGN IN</Link>
             }
+            <CartIcon />
         </div>
     </div>
 );
