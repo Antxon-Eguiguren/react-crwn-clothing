6ff2f66986a72e50a0426ef066bcb18cfbd43db3
Antxon Eguiguren <antxonegi@gmail.com>
2020-04-15 12:20:52 +0200

    mapStateToProps and mapDispatchToProps implemented in AppComponent and HeaderComponent

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,7 @@ import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
-// Set up the Middlewares array. In this case we only include the logger middleware, but we can include more...
+// Set up the Middlewares array. In this case we only include the logger middleware, but we can include more...It shows the previous state, action and next state of the store
 const middlewares = [logger];
 
 // Set up the redux store
