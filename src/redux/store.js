22c6550e0e4d5cf15b7504adc6ca5be2c7e338fc
Antxon Eguiguren <antxonegi@gmail.com>
2020-04-15 10:32:10 +0200

    Redux installed and root reducerm user reducer and store configured

---
+++ src/redux/store.js
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+
+import rootReducer from './root-reducer';
+
+// Set up the Middlewares array. In this case we only include the logger middleware, but we can include more...
+const middlewares = [logger];
+
+// Set up the redux store
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+//In case we apply only the logger middleware, but the other way is more escalable.
+// const store = createStore(reactRedcuer, applyMiddleware(logger));
+
+export default store;
---
+++ src/redux/user/user.reducer.js
@@ -0,0 +1,18 @@
+const INITAL_STATE = {
+    currentUser: null
+}
+
+// Default value for state, in case state is not set
+const userReducer = (state = INITAL_STATE, action) => {
+    switch (action.type) {
+        case 'SET_CURRENT_USER':
+            return {
+                ...state,
+                currentUser: action.payload
+            }
+        default:
+            return state;
+    }
+}
+
+export default userReducer;
