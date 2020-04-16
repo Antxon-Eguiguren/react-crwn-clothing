298b1dc92701c8b9ef6eeb9a65a15f8a26870ef6
Antxon Eguiguren <antxonegi@gmail.com>
2020-04-16 11:57:45 +0200

    AddItem action created in CartReducer, not finished

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,7 +1,8 @@
 import CartActionTypes from './cart.types';
 
 const INITAL_STATE = {
-    hidden: true
+    hidden: true,
+    cartItems: []
 };
 
 const cartReducer = (state = INITAL_STATE, action) => {
@@ -11,6 +12,13 @@ const cartReducer = (state = INITAL_STATE, action) => {
                 ...state,
                 hidden: !state.hidden
             }
+
+        case CartActionTypes.ADD_ITEM:
+            return {
+                ...state,
+                cartItems: [...state.cartItems, action.payload]
+            }
+
         default:
             return state;
     }
