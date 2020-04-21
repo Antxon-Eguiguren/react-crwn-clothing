cf333f52ca9dbd29594e881b2d398c0393cf32db
Antxon Eguiguren <antxonegi@gmail.com>
2020-04-21 20:51:48 +0200

    Fetching data from Firebase from ShopPage in an asynchronous way, using Redux Thunk

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,19 +1,32 @@
 import { createSelector } from 'reselect';
 
+// Input selector type 1: selects a part of the state
 const selectShop = (state) => state.shop;
 
+// Input selector type 2: selects a part of an input selector type 1
 export const selectCollections = createSelector(
     [selectShop],
     shop => shop.collections
 );
 
+// Output selectors: outputs from redux to the app the selected data, to be used in the mapStateToProps of the components
 export const selectCollectionsForPreview = createSelector(
     [selectCollections],
-    // Gets the keys of the object in a form of an array
-    collections => (collections ? Object.keys(collections).map(key => collections[key]) : [])
+    collections => collections ? Object.keys(collections).map(item => collections[item]) : []
 );
 
 export const selectCollection = (collectionUrlParam) => createSelector(
     [selectCollections],
     collections => (collections ? collections[collectionUrlParam] : null)
+);
+
+export const selectIsCollectionFetching = createSelector(
+    [selectShop],
+    shop => shop.isFetching
+);
+
+export const selectIsCollectionsLoaded = createSelector(
+    [selectShop],
+    // !! converts an object into true/false value (if object is null then returns false, if not null (including an empty object), then returns true) 
+    shop => !!shop.collections
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 import rootReducer from './root-reducer';
 
-// Set up the Middlewares array. In this case we only include the logger middleware in the development version of the app (not in production), but we can include more...It shows the previous state, action and next state of the store
-const middlewares = [];
+// Set up the Middlewares array. In this case we include the logger middleware in the development version of the app (not in production). It shows the previous state, action and next state of the store. Also, we included the thunk middleware that allows to make asynchronous calls in redux
+const middlewares = [thunk];
 if (process.env.NODE_ENV === 'development') {
     middlewares.push(logger);
 }
